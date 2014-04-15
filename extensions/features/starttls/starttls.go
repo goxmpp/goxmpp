5fb8182c237b633fb6375c1421c159f19a63be3d
Alexander (Alex) Zhavnerchik <alex.vizor@gmail.com>
2014-04-15 20:21:06 +0300

    First NOT TESTED starttls implementation

--- extensions/features/starttls/starttls.go
+++ extensions/features/starttls/starttls.go
@@ -1,7 +1,12 @@
 package starttls
 
 import (
+	"crypto/tls"
 	"encoding/xml"
+	"errors"
+	"io"
+	"log"
+	"net"
 
 	"github.com/dotdoom/goxmpp/stream"
 	"github.com/dotdoom/goxmpp/stream/elements"
@@ -16,21 +21,63 @@ func init() {
 }
 
 type StartTLSFeatureElement struct {
-	XMLName  xml.Name `xml:"urn:ietf:params:xml:ns:xmpp-tls starttls"`
-	Required bool     `xml:"required,omitempty"`
+	Required        bool `xml:"required,omitempty"`
+	StartTLSElement      // will get XMLName from here
 }
 
-func NewStartTLSFeature() *StartTLSFeatureElement {
-	return &StartTLSFeatureElement{}
+func NewStartTLSFeature(required bool) *StartTLSFeatureElement {
+	return &StartTLSFeatureElement{Required: required}
+}
+
+func (s *StartTLSFeatureElement) CopyIfAvailable(s *stream.Stream) elements.Element {
+	// Handcoded for now because this will be reworked anyway
+	return NewStartTLSFeature(true)
 }
 
 type StartTLSElement struct {
 	XMLName xml.Name `xml:"urn:ietf:params:xml:ns:xmpp-tls starttls"`
 }
 
-func (s *StartTLSElement) Handle(stream *stream.Stream) error {
-	stream.WriteElement(&ProceedElement{})
-	return nil
+type TSLConfig struct {
+	PEMPath string
+	KeyPath string
+}
+
+type StartTLSState struct {
+	Started bool
+	Config  TLSConfig
+}
+
+func (s *StartTLSElement) Handle(s *stream.Stream) error {
+	var state *StartTLSState
+	if err := s.State.Get(&state); err != nil {
+		return err
+	}
+
+	cert, err := tls.LoadX509KeyPair(state.Config.PEMPath, state.Config.KeyPath)
+	if err != nil {
+		log.Println("Could not load keys:", err)
+		return err
+	}
+
+	config := tls.Config{Certificates: []tls.Certificate{cert}, ClientAuth: tls.RequireAnyClientCert}
+
+	err = s.UpdateRW(func(srwc io.ReadWriteCloser) (io.ReadWriteCloser, error) {
+		if conn, ok := srwc.(net.Conn); ok {
+			tls_conn, err := tls.Server(conn, config)
+			if err != nil {
+				return nil, err
+			}
+			s.WriteElement(&ProceedElement{})
+			return tls_conn, nil
+		}
+		return nil, errors.New("Wrong ReadWriteCloser, expected connection")
+	})
+	if err != nil {
+		log.Println("Could not replace connection", err)
+	}
+
+	return err
 }
 
 type ProceedElement struct {
